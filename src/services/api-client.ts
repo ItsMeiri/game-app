494c24fa1f6eb4ec3cd3dbed8abbc5e0b5c23235
Daniel Meiri <danielt.meiri@gmail.com>
2023-09-06 13:12:24 +0300

    create custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const controller = new AbortController();
+
 
 export default axios.create({
-    signal: controller.signal,
+  
     baseURL: "https://api.rawg.io/api",
     params: {
         key: 'a5cdbb9a28b844a39a938dabbba3e8cb'
