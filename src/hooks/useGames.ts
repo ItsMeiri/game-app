8561efce3e4e2bf26856d11567b2a22cfa71390d
Daniel Meiri <danielt.meiri@gmail.com>
2023-09-11 13:39:04 +0300

    add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,28 +3,27 @@ import useData from "./useData";
 
 import { Platform } from "./usePlatforms";
 
-
-
 export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[]
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
+const useGames = (gameQuery: GameQuery) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchTerm,
+      },
+    },
+    [gameQuery]
+  );
 
-
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
-  params: {
-    genres: gameQuery.genre?.id,
-    platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchTerm
-    }
-    }, [gameQuery])
-
-export default useGames
-
-
-
+export default useGames;
