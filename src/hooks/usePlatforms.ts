2f4e22f71c3615e48a2dc6801c25063a07d78102
Daniel Meiri <danielt.meiri@gmail.com>
2023-09-11 13:45:12 +0300

    make genre and platform lists static

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,14 @@
-import useData from "./useData";
-
+import platforms from "../data/platforms";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-  }
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+  id: number;
+  name: string;
+  slug: string;
+}
+const usePlatforms = () => ({
+  data: platforms,
+  error: null,
+  isLoading: false,
+});
 
-export default usePlatforms
+export default usePlatforms;
